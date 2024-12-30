02360ca81a256fdf959fa3665a90a00db1fc0701
Ahmed Nabi Khan <26043220+ahmednabik@users.noreply.github.com>
2024-12-31 01:16:57 +0500

    configured health check path

--- app/api/health-check/route.ts
+++ app/api/health-check/route.ts
@@ -1,2 +1,10 @@
+import { NextResponse } from 'next/server'
 
-
+export async function GET() {
+  return new Response("OK", {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  })
+}
